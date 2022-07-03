240935f389030583a0a72e3987f04a2dcb4bf768
Arsenii <99754008+magistar0@users.noreply.github.com>
2022-07-03 12:48:11 +0300

    версия 2022-07-03 - 1

---
+++ data_parser.py
@@ -0,0 +1,107 @@
+with open('data/tasks_data/1/1_data.txt', 'r', encoding='utf-8') as data_1_file:
+    task_1_dict = eval(data_1_file.read())
+    task_1_list_of_dicts = task_1_dict['tasks']
+
+with open('data/tasks_data/2/2_data.txt', 'r', encoding='utf-8') as data_2_file:
+    task_2_dict = eval(data_2_file.read())
+    task_2_list_of_dicts = task_2_dict['tasks']
+
+with open('data/tasks_data/3/3_data.txt', 'r', encoding='utf-8') as data_3_file:
+    task_3_dict = eval(data_3_file.read())
+    task_3_list_of_dicts = task_3_dict['tasks']
+
+with open('data/tasks_data/4/4_data.txt', 'r', encoding='utf-8') as data_4_file:
+    task_4_dict = eval(data_4_file.read())
+    task_4_list_of_dicts = task_4_dict['tasks']
+
+with open('data/tasks_data/5/5_data.txt', 'r', encoding='utf-8') as data_5_file:
+    task_5_dict = eval(data_5_file.read())
+    task_5_list_of_dicts = task_5_dict['tasks']
+
+with open('data/tasks_data/6/6_data.txt', 'r', encoding='utf-8') as data_6_file:
+    task_6_dict = eval(data_6_file.read())
+    task_6_list_of_dicts = task_6_dict['tasks']
+
+with open('data/tasks_data/7/7_data.txt', 'r', encoding='utf-8') as data_7_file:
+    task_7_dict = eval(data_7_file.read())
+    task_7_list_of_dicts = task_7_dict['tasks']
+
+with open('data/tasks_data/8/8_data.txt', 'r', encoding='utf-8') as data_8_file:
+    task_8_dict = eval(data_8_file.read())
+    task_8_list_of_dicts = task_8_dict['tasks']
+
+with open('data/tasks_data/9/9_data.txt', 'r', encoding='utf-8') as data_9_file:
+    task_9_dict = eval(data_9_file.read())
+    task_9_list_of_dicts = task_9_dict['tasks']
+
+with open('data/tasks_data/10/10_data.txt', 'r', encoding='utf-8') as data_10_file:
+    task_10_dict = eval(data_10_file.read())
+    task_10_list_of_dicts = task_10_dict['tasks']
+
+with open('data/tasks_data/11/11_data.txt', 'r', encoding='utf-8') as data_11_file:
+    task_11_dict = eval(data_11_file.read())
+    task_11_list_of_dicts = task_11_dict['tasks']
+
+with open('data/tasks_data/12/12_data.txt', 'r', encoding='utf-8') as data_12_file:
+    task_12_dict = eval(data_12_file.read())
+    task_12_list_of_dicts = task_12_dict['tasks']
+
+with open('data/tasks_data/13/13_data.txt', 'r', encoding='utf-8') as data_13_file:
+    task_13_dict = eval(data_13_file.read())
+    task_13_list_of_dicts = task_13_dict['tasks']
+
+with open('data/tasks_data/14/14_data.txt', 'r', encoding='utf-8') as data_14_file:
+    task_14_dict = eval(data_14_file.read())
+    task_14_list_of_dicts = task_14_dict['tasks']
+
+with open('data/tasks_data/15/15_data.txt', 'r', encoding='utf-8') as data_15_file:
+    task_15_dict = eval(data_15_file.read())
+    task_15_list_of_dicts = task_15_dict['tasks']
+
+with open('data/tasks_data/16/16_data.txt', 'r', encoding='utf-8') as data_16_file:
+    task_16_dict = eval(data_16_file.read())
+    task_16_list_of_dicts = task_16_dict['tasks']
+
+with open('data/tasks_data/17/17_data.txt', 'r', encoding='utf-8') as data_17_file:
+    task_17_dict = eval(data_17_file.read())
+    task_17_list_of_dicts = task_17_dict['tasks']
+
+with open('data/tasks_data/18/18_data.txt', 'r', encoding='utf-8') as data_18_file:
+    task_18_dict = eval(data_18_file.read())
+    task_18_list_of_dicts = task_18_dict['tasks']
+
+with open('data/tasks_data/19/19_data.txt', 'r', encoding='utf-8') as data_19_file:
+    task_19_dict = eval(data_19_file.read())
+    task_19_list_of_dicts = task_19_dict['tasks']
+
+with open('data/tasks_data/20/20_data.txt', 'r', encoding='utf-8') as data_20_file:
+    task_20_dict = eval(data_20_file.read())
+    task_20_list_of_dicts = task_20_dict['tasks']
+
+with open('data/tasks_data/21/21_data.txt', 'r', encoding='utf-8') as data_21_file:
+    task_21_dict = eval(data_21_file.read())
+    task_21_list_of_dicts = task_21_dict['tasks']
+
+with open('data/tasks_data/22/22_data.txt', 'r', encoding='utf-8') as data_22_file:
+    task_22_dict = eval(data_22_file.read())
+    task_22_list_of_dicts = task_22_dict['tasks']
+
+with open('data/tasks_data/23/23_data.txt', 'r', encoding='utf-8') as data_23_file:
+    task_23_dict = eval(data_23_file.read())
+    task_23_list_of_dicts = task_23_dict['tasks']
+
+with open('data/tasks_data/24/24_data.txt', 'r', encoding='utf-8') as data_24_file:
+    task_24_dict = eval(data_24_file.read())
+    task_24_list_of_dicts = task_24_dict['tasks']
+
+with open('data/tasks_data/25/25_data.txt', 'r', encoding='utf-8') as data_25_file:
+    task_25_dict = eval(data_25_file.read())
+    task_25_list_of_dicts = task_25_dict['tasks']
+
+with open('data/tasks_data/26/26_data.txt', 'r', encoding='utf-8') as data_26_file:
+    task_26_dict = eval(data_26_file.read())
+    task_26_list_of_dicts = task_26_dict['tasks']
+
+with open('data/tasks_data/27/27_data.txt', 'r', encoding='utf-8') as data_27_file:
+    task_27_dict = eval(data_27_file.read())
+    task_27_list_of_dicts = task_27_dict['tasks']
