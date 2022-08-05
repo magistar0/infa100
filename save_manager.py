524b00df9e54854741551a3cbf0159211396daf9
Arsenii <99754008+magistar0@users.noreply.github.com>
2022-08-05 23:00:14 +0300

    переработка сейвов

--- save_manager.py
+++ save_manager.py
@@ -1,8 +1,11 @@
 import json
 import datetime
+import os
+
+dir_path = '%s/INFA100/' %  os.environ['APPDATA']
 
 def read_save() -> dict:
-    with open('data/config/user_save.save', 'r', encoding='utf-8') as save_file:
+    with open(dir_path + 'user_save.save', 'r', encoding='utf-8') as save_file:
         save_data = json.loads(save_file.read())
         return save_data
 
@@ -10,14 +13,14 @@ def add_id_to_save(task, id) -> None:
     save_data = read_save()
     save_data['save_data'][str(task)].append(str(id))
     save_data['update_time'] = str(datetime.datetime.now())
-    with open('data/config/user_save.save', 'w', encoding='utf-8') as save_file:
+    with open(dir_path + 'user_save.save', 'w', encoding='utf-8') as save_file:
         save_file.write(json.dumps(save_data, ensure_ascii=False))
 
 def generate_empty_save() -> None:
     save_data = {}
     for task in range(1, 28):
         save_data[task] = []
-    with open('data/config/user_save.save', 'w', encoding='utf-8') as sf:
+    with open(dir_path + 'user_save.save', 'w', encoding='utf-8') as sf:
         sf.write(json.dumps({"save_data": save_data, 'update_time': str(datetime.datetime.now())}, ensure_ascii=False))
 
 def check_id_in_save(task, id) -> bool:
@@ -28,7 +31,7 @@ def clear_task_data_in_save(task) -> None:
     save_data = read_save()
     save_data['save_data'][str(task)] = []
     save_data['update_time'] = str(datetime.datetime.now())
-    with open('data/config/user_save.save', 'w', encoding='utf-8') as save_file:
+    with open(dir_path + 'user_save.save', 'w', encoding='utf-8') as save_file:
         save_file.write(json.dumps(save_data, ensure_ascii=False))
 
 def get_save_data_for_task(task) -> list:
